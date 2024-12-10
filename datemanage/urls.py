104acd1c586248ca4456457e3fb927aa7d8c9bbb
xinlong <1852344985@qq.com>
2024-12-10 22:19:41 +0800

    123

--- datemanage/urls.py
+++ datemanage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'datemanage'
-#123
+
 urlpatterns = [
     path('datemanage/', views.datemanage, name='datemanage'),  # 员工信息管理
 
--- datemanage/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def datemanage(request):
-    return HttpResponse("这是日常事务管理页面")
